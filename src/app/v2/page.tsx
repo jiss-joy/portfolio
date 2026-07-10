b4ea07e99724cb4e1324fad07678432aa05911f4
Jiss Joy <jissjoy1@gmail.com>
2026-07-10 16:19:07 +0530

    responsive

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -4,13 +4,13 @@ import { SakuraPetals } from '@/components/v2/sakura-petals'
 
 export default function Home() {
   return (
-    <div className="relative h-screen overflow-hidden bg-background">
+    <div className="relative min-h-screen bg-background lg:h-screen lg:overflow-hidden">
       <SakuraPetals />
-      <main className="relative z-10 flex h-full w-full">
-        <div className="flex h-full min-w-0 flex-1 justify-end">
+      <main className="relative z-10 flex w-full flex-col lg:h-full lg:flex-row">
+        <div className="w-full lg:flex lg:h-full lg:min-w-0 lg:flex-1 lg:justify-end">
           <LeftSection />
         </div>
-        <div className="flex h-full min-w-0 flex-1">
+        <div className="w-full lg:flex lg:h-full lg:min-w-0 lg:flex-1">
           <RightSection />
         </div>
       </main>
